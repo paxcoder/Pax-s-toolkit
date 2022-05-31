3cebcd224ef1f5bab35e4d3e8c26530adb3eccae
unknown <paxkub@outlook.com>
2022-05-31 19:17:24 +0200

    version 0.2 is out, added checking for windows version.

--- globalPlugins/Pax's_toolkit.py
+++ globalPlugins/Pax's_toolkit.py
@@ -3,9 +3,16 @@ import gui
 import scriptHandler
 import random
 import ui
+import config
+import winVersion
 class GlobalPlugin(globalPluginHandler.GlobalPlugin):
     @scriptHandler.script(gesture="kb:NVDA+control+D", description="rolls the dice", category="pax's toolkit")
     def script_dice(self,gesture):
      roll=random.randint(1,6)
      converted=str(roll)
      ui.message(converted)
+    @scriptHandler.script(gesture="kb:NVDA+v", description="checks windows version", category="Pax's toolkit")
+    def script_getWinVersion(self,gesture):
+        v=winVersion.getWinVer()
+        ver=str(v)
+        ui.message(ver)
