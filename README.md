e578f56b2684a1c229d68d26483a3a94ce9d0e75
unknown <paxkub@outlook.com>
2022-06-02 10:04:08 +0200

    added: normal readme. fixed: 2 categories named the same in input gestures but having different options.

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Pax-s-toolkit
 an addon for NVDA that has some functionalities, maybe usefull for you, maybe not.
+##features
+roll a die, currently shortcut is broken), but you can assign one in Pax's toolkit's section in input gestures dialog.
+##=>0.2 functionalities
+check system version(nvda+v
--- globalPlugins/Pax's_toolkit.py
+++ globalPlugins/Pax's_toolkit.py
@@ -6,7 +6,7 @@ import ui
 import config
 import winVersion
 class GlobalPlugin(globalPluginHandler.GlobalPlugin):
-    @scriptHandler.script(gesture="kb:NVDA+control+D", description="rolls the dice", category="pax's toolkit")
+    @scriptHandler.script(gesture="kb:NVDA+control+D", description="rolls the dice", category="Pax's toolkit")
     def script_dice(self,gesture):
      roll=random.randint(1,6)
      converted=str(roll)
